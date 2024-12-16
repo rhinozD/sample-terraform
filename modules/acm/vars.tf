0b268004e43dd1b12b9aca87689ae349977f95a1
duong.nguyenvan <duong.nguyenvan@smartdev.com>
2024-12-16 23:06:26 +0700

    update terraform templates and github actions workflow

--- modules/acm/vars.tf
+++ modules/acm/vars.tf
@@ -1,18 +1,15 @@
 variable "domain_name" {
   type        = string
-  default     = ""
   description = "A domain name for which the certificate should be issued"
 }
 
 variable "zone_id" {
   type        = string
-  default     = ""
   description = "The ID of the hosted zone to contain this record. Required when validating via Route53"
 }
 
 variable "subject_alternative_names" {
   type        = list(string)
-  default     = [""]
   description = "A list of domains that should be SANs in the issued certificate"
 }
 
